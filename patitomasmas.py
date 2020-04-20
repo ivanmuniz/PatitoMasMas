7ac02ac6837fa8076c4eab148917ad723ef9ebc5
Iván Muñiz <ivanmr_96@live.com.mx>
2020-04-20 13:14:33 -0500

    Finished parser

--- patitomasmas.py
+++ patitomasmas.py
@@ -30,7 +30,8 @@ tokens = [
     'ID', 'MINUS', 'PLUS', 'TIMES', 'DIVIDE',
     'SEMICOLON', 'OPENPAREN', 'CLOSEPAREN',
     'COMMA', 'OPENCURL', 'CLOSECURL', 'COLON', 
-    'EQUAL', 'GREATERTHAN', 'LESSTHAN', 'NOTEQUAL',
+    'EQUAL', 'GREATERTHAN', 'LESSTHAN', 'NOTEQUAL', 'CEQUAL', 'GREATEROREQUAL', 'LESSEROREQUAL',
+    'AND', 'OR',
     'CTEI', 'CTEF', 'CTESTRING', 'CTECHAR',
     'LBRACKET', 'RBRACKET'
 ] + list(reserved.values())
@@ -49,7 +50,12 @@ t_CLOSECURL = r'\}'
 t_COLON = r'\:'
 t_GREATERTHAN = r'\>'
 t_LESSTHAN = r'\<'
+t_CEQUAL = r'\=='
 t_NOTEQUAL = r'\!='
+t_GREATEROREQUAL = r'\>='
+t_LESSEROREQUAL = r'\<='
+t_AND = r'\&&'
+t_OR = r'\|\|'
 t_LBRACKET = r'\['
 t_RBRACKET = r'\]'
 
@@ -58,7 +64,7 @@ t_ignore  = ' \t\n'
 
 # Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
+    print("Illegal character: %s" % t.value[0])
     t.lexer.skip(1)
 
 def t_CTEF(t):
@@ -71,13 +77,14 @@ def t_CTEI(t):
     t.value = int(t.value)
     return t
 
-def t_CTESTRING(t):
-    r'\'[\w\d\s\,. ]*\'|\"[\w\d\s\,. ]*\"'
+def t_CTECHAR(t):
+    r'\'[\w]\'|"[\w]"'
     return t
 
-def t_CTECHAR(t):
-    r'\'[\w]\''
+def t_CTESTRING(t):
+    r'"[\w\d\s\,. ]*"|\'[\w\d\s\,. ]*\''
     return t
+
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z_\d]*'
@@ -128,7 +135,7 @@ def p_tipo(p):
 # ------------------------------------ Funciones ------------------------------------
 def p_funciones(p):
     '''
-        funciones : FUNCION retorno ID OPENPAREN parametros_funcion CLOSEPAREN declaraciones bloque
+        funciones : FUNCION retorno ID OPENPAREN parametros_funcion CLOSEPAREN declaraciones bloque funciones
             | empty     
     '''
 
@@ -154,6 +161,147 @@ def p_retorno(p):
 def p_bloque(p):
     '''
         bloque : OPENCURL CLOSECURL
+            | OPENCURL estatutosprime CLOSECURL
+    '''
+
+def p_estatutosprime(p):
+    '''
+        estatutosprime : estatutos
+            | estatutos estatutosprime
+    '''
+
+def p_estatutos(p):
+    '''
+        estatutos : asignacion
+            | condicion
+            | escritura
+            | retorno_de_funcion
+            | lectura
+            | rep_no_cond
+            | rep_cond
+            | llamada_a_funcion_void
+    '''
+
+def p_asignacion(p):
+    '''
+        asignacion : variable EQUAL exp SEMICOLON
+    '''
+
+def p_variable(p):
+    '''
+        variable : ID
+            | ID LBRACKET exp RBRACKET
+            | ID LBRACKET exp RBRACKET LBRACKET exp RBRACKET
+    '''
+
+def p_expresion(p):
+    '''
+        expresion : exp
+            | exp AND expresion
+            | exp OR expresion
+    '''
+
+def p_exp(p):
+    '''
+        exp : exp_ar
+            | exp_ar GREATERTHAN exp_ar
+            | exp_ar LESSTHAN exp_ar
+            | exp_ar GREATEROREQUAL exp_ar
+            | exp_ar LESSEROREQUAL exp_ar
+            | exp_ar CEQUAL exp_ar
+            | exp_ar NOTEQUAL exp_ar
+    '''
+
+def p_exp_ar(p):
+    '''
+        exp_ar : termino
+            | termino PLUS exp_ar
+            | termino MINUS exp_ar
+    '''
+
+def p_termino(p):
+    '''
+        termino : factor
+            | factor TIMES termino
+            | factor DIVIDE termino
+    '''
+
+def p_factor(p):
+    '''
+        factor : var_cte
+            | variable
+            | llamada_a_funcion
+            | OPENPAREN exp CLOSEPAREN
+    '''
+
+def p_var_cte(p):
+    '''
+        var_cte : CTEI
+            | CTEF
+            | CTECHAR
+    '''
+
+def p_condicion(p):
+    '''
+        condicion : SI OPENPAREN expresion CLOSEPAREN ENTONCES bloque
+            | SI OPENPAREN expresion CLOSEPAREN ENTONCES bloque SINO bloque
+    '''
+
+def p_escritura(p):
+    '''
+        escritura : ESCRIBE OPENPAREN escrituraprime CLOSEPAREN SEMICOLON
+    '''
+
+def p_escrituraprime(p):
+    '''
+        escrituraprime : expresion
+            | expresion COMMA escrituraprime
+            | CTESTRING
+            | CTESTRING COMMA escrituraprime
+
+    '''
+
+def p_retorno_de_funcion(p):
+    '''
+        retorno_de_funcion : REGRESA OPENPAREN exp CLOSEPAREN SEMICOLON
+    '''
+
+def p_lectura(p):
+    '''
+        lectura : LEE OPENPAREN lecturaprime CLOSEPAREN SEMICOLON
+    '''
+
+def p_lecturaprime(p):
+    '''
+        lecturaprime : variable
+            | variable COMMA lecturaprime
+    '''
+
+def p_rep_no_cond(p):
+    '''
+        rep_no_cond : DESDE variable EQUAL exp HASTA exp HACER bloque
+    '''
+
+def p_rep_cond(p):
+    '''
+        rep_cond : MIENTRAS OPENPAREN exp CLOSEPAREN HAZ bloque
+    '''
+
+def p_llamada_a_funcion(p):
+    '''
+        llamada_a_funcion : ID OPENPAREN CLOSEPAREN
+            | ID OPENPAREN argumentos_funcion CLOSEPAREN
+    '''
+
+def p_argumentos_funcion(p):
+    '''
+        argumentos_funcion : tipo ID
+            |   tipo ID COMMA argumentos_funcion
+    '''
+
+def p_llamada_a_funcion_void(p):
+    '''
+        llamada_a_funcion_void : llamada_a_funcion SEMICOLON
     '''
 
 def p_empty(p):
