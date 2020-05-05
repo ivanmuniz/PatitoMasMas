7bd031ca306cc7e462bd7d5403fb31eeda83fdda
Ricardo <ricardo.acosta1809@gmail.com>
2020-05-05 18:10:36 -0500

    Create semantic cube

--- semantic-cube.py
+++ semantic-cube.py
@@ -1,1 +1,113 @@
 class SemanticCube:
+    def __init__(self):
+        self.cube = {
+            '+': {
+                'int':{
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'int',
+                },
+                'float': {
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                },
+                'str': {
+                    'int': 'err',
+                    'float': 'err',
+                    'str': 'str',
+                    'bool': 'err'
+                },
+                'bool': {
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'int'
+                }
+            },
+            '-': {
+                'int': {
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'int'
+                },
+                'float': {
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                },
+                'str': {
+                    'int': 'err',
+                    'float': 'err',
+                    'str': 'err',
+                    'bool': 'err'
+                },
+                'bool':{
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'err'
+                }
+            },
+            '/': {
+                'int': {
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                },
+                'float': {
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                },
+                'str': {
+                    'int': 'err',
+                    'float': 'err',
+                    'str': 'str',
+                    'bool': 'float'
+                },
+                'bool':{
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                }
+            },
+            '*': {
+                'int': {
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'str',
+                    'bool': 'int'
+                },
+                'float': {
+                    'int': 'float',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'float'
+                },
+                'str': {
+                    'int': 'str',
+                    'float': 'err',
+                    'str': 'err',
+                    'bool': 'err'
+                },
+                'bool':{
+                    'int': 'int',
+                    'float': 'float',
+                    'str': 'err',
+                    'bool': 'err'
+                }
+            },
+            # TODO: Faltan los condicionales
+            '>': {},
+            '<': {},
+            '&&': {},
+            '||': {}
+        }
